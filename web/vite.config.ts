e1045e392beb982a38da423960951a90128ee490
qhcchina666 <qhcchina666@outlook.com>
2026-08-15 10:38:09 +0800

    Update vite.config.ts

--- web/vite.config.ts
+++ web/vite.config.ts
@@ -1,12 +1,13 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
-import { VitePWA } from 'vite-plugin-pwa'
+// import { VitePWA } from 'vite-plugin-pwa'
 
 export default defineConfig({
   plugins: [
     react(),
     tailwindcss(),
+    /*
     VitePWA({
       registerType: 'autoUpdate',
       workbox: {
@@ -55,6 +56,7 @@ export default defineConfig({
         ],
       },
     }),
+    */
   ],
   server: { host: true },
 })
